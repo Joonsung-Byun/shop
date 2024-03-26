cf39f60317c2e0dec5f5236991d300ea90c2cec1
Joonsung-Byun <122327073+Joonsung-Byun@users.noreply.github.com>
2024-03-25 23:23:02 -0600

    Revert "backup"
    This reverts commit 9b9e4dc57109260522aa7d1684c2861bf304d0be.

--- src/App.js
+++ src/App.js
@@ -1,134 +1,78 @@
 import "./App.css";
-import {Navbar, Container, Nav} from 'react-bootstrap';
-import bg from './img/bg.png';
-import { createContext, useEffect, useState } from "react";
-import data from './data.js';
-import { Link, Routes, Route, useNavigate, Outlet } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 import Detail from "./pages/detail.js";
-import Event from "./pages/event.js";
+import Navigation from "./nav.js";
+import Event from "./pages/event";
+import data from "./data";
+import { useState } from "react";
+import Page404 from "./pages/Page404";
+import bg from "./img/bg.png";
+import { Container, Row, Col } from "react-bootstrap";
 import axios from "axios";
-import Cart from "./pages/Cart.js";
-import Seen from "./pages/Seen.js";
-
-
-
-//context란 state 보관함
+// import { useEffect } from "react";
 
 function App() {
+  let [shoes, setShoes] = useState(data);
+  let [count, setCount] = useState(1);
+  console.log(count)
+  // let [more, setMore] = useState(false);
 
-
-  useEffect(()=>{
-    const options = {
-      method: 'GET',
-      url: 'https://api-football-v1.p.rapidapi.com/v3/fixtures?',
-      params: {
-        next: '50'
-      },
-      headers: {
-        'X-RapidAPI-Key': '67142be9e1msha21067b17d884d6p1f558ejsn8dc79fea8402',
-        'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
-      }
-    };
-    
-    try {
-      const fetchData = async () => {
-        const response = await axios.request(options);
-        console.log(response.data.response);
-      };
-
-      fetchData();
-    } catch (error) {
-      console.error(error);
-    }
-
-
-
-  //   let watchedValue = localStorage.getItem('watched');
-  //   if (watchedValue !== null && watchedValue !== undefined) {   
-  //     // 'watched' 키의 값이 있는 경우 처리할 내용을 여기에 작성합니다.
-  //     watchedValue = JSON.parse(watchedValue)
-  //     let foundIndex = watchedValue.length;
-      
-  // } else {
-  //     // 'watched' 키의 값이 없는 경우 처리할 내용을 여기에 작성합니다.
-  //     localStorage.setItem('watched', JSON.stringify( [] ))
-
-  // }
-  },[])
-
-  let [shoes,setShoes] = useState(data);
-  // let [stock] = useState([10,11,12]);
-  let navigate = useNavigate();
-  let [num, setNum] = useState(2);
+  // useEffect(() => {
+  //   axios
+  //     .get("https://codingapple1.github.io/shop/data2.json")
+  //     .then((response) => {
+  //       let newShoes = response.data;
+  //       shoes.push.apply(shoes, newShoes);
+  //     });
+  // }, [more]);
 
   return (
     <div className="App">
-
-      <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="/">ShoeShop</Navbar.Brand>
-          <Nav className="me-auto">
-          <Nav.Link onClick={()=>{ navigate('/') }}>Home</Nav.Link>
-          <Nav.Link onClick={()=>{ navigate('/detail') }}>Detail</Nav.Link>
-          <Nav.Link onClick={()=>{ navigate('/event') }}>Event</Nav.Link>
-          <Nav.Link onClick={()=>{ navigate('/cart') }}>Cart</Nav.Link>
-          <Nav.Link onClick={()=>{ navigate('/seen') }}>Recently Viewed</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-
+      <Navigation></Navigation>
       <Routes>
         <Route path="/" element={
-        <div>
-          <div className="main-bg" style={{backgroundImage : 'url(' + bg + ')'}}></div>
-          <div className="container">
-            <div className="row">
-              {
-              shoes.map(function(a, i){
-                return <Cards shoes={shoes} i={i} key={i} />
-                })
-              }
-            </div>
-            <button onClick={()=>{
-              console.log('hi');
-              let newShoes = [...shoes];
-              newShoes.sort(function (a, b){
-                return a.title < b.title? -1 : a.title > b.title? 1 : 0;
-              })
-              console.log(newShoes)
-              setShoes(newShoes);
-              console.log('bye');
-            }}>Sort Shoes</button>
+            <>
+              <div className="main-bg" style={{ backgroundImage: "url(" + bg + ")" }}></div>
+              <div>
+                <Container>
+                  <Row>
+                    {shoes.map(function (a, i) {
+                      const imageUrl = `https://codingapple1.github.io/shop/shoes${ i + 1 }.jpg`;
+                      return (
+                        <Col sm key={i}>
+                          <img src={imageUrl} width="80%" />
+                          <h4>{shoes[i].title}</h4>
+                          <p>{shoes[i].price}</p>
+                        </Col>);})}
+                  </Row>
+                  <button onClick={() => { 
+                    axios.get(`https://codingapple1.github.io/shop/data${count + 1}.json`)
+                    .then(response => { 
+                      let copy = [...shoes, ...response.data]
+                      console.log(copy)
+                      setShoes(copy);
+                    })
+                    .then(()=>{
+                      setCount(count+1)
+                      console.log(count)
+                    })
+                    .catch(error => {
+                      alert('신발이 더이상 없습니다.'); // URL을 불러오지 못했을 때 오류 메시지를 표시할 수 있습니다.
+                    });
+                   }}> 더보기 </button>
+                </Container>
+              </div>
+            </>}>
+          </Route> 
+          <Route path="/detail/:id" element={<Detail shoes={shoes}/>} />
 
-            <button onClick={()=>{
-              setNum(num+1);
-              if(num>3){
-                alert('더이상 상품이 없습니다.');
-              } else {
-              axios.get(`https://codingapple1.github.io/shop/data${num}.json`)
-              .then((result)=>{ 
-                let copy = [...shoes, ...result.data]
-                setShoes(copy)
-                ;})
-              }              
-            }}>상품더보기</button>
-          
+          <Route path="event" element={<Event />}>
+            <Route path="one" element={<p>첫 주문시 양배추집 서비스</p>}></Route>
+            <Route path="two" element={<p>생일기념 쿠폰받기</p>}></Route>
+          </Route>
 
-          
-          </div>
-        </div>}/>
-        <Route path="/detail" element={ <div>Detail 페이지임~</div> }></Route>
-          <Route path="/detail/:id" element={ <Detail shoes={shoes}/> }></Route>
-        <Route path="/event" element={ <Event /> }>
-            <Route path="one" element={ <div> 첫 주문시 양배추즙 서비스 </div> }/>
-            <Route path="two" element={ <div> 생일기념 쿠폰받기 </div> }/>
-        </Route>
-        <Route path="/cart" element={ <Cart/> }/>
-        <Route path="/seen" element={ <Seen shoes={shoes} setShoes={setShoes}/> }/>
-        <Route path="*" element={ <div> 404페이지임~~ </div> }/>
+          <Route path="*" element={<Page404 error={"404인디 ㅅㄱㅋㅋ"} />} />
       </Routes>
-
-      
     </div>
   );
 }
